a8321b03f8baaa70429a5108ee0ce914b89adc2d
VictorApaez <vic.apaez@gmail.com>
2023-08-03 14:52:01 -0400

    created test cases for test_neighboring_mines_count

--- tests/test_board.py
+++ tests/test_board.py
@@ -9,3 +9,33 @@ def test_mine_placement():
 
     assert mine_count == mines, f"Expected {mines} mines, but got {mine_count}"
 
+def test_neighboring_mines_count():
+    # no mines
+    rows, columns, mines = 5, 5, 0
+    board = Board(rows, columns, mines)
+
+    for row in board.board:
+        for cell in row:
+            assert cell.neighboring_mines == 0, "Expected 0 neighboring mines for an empty board"
+
+    board.board[0][0].is_mine = True
+    # ^^ 1 mine at (0,0)
+    # [
+    #   [x, _, _, _, _],
+    #   [_, _, _, _, _],
+    #   [_, _, _, _, _],
+    #   [_, _, _, _, _],
+    #   [_, _, _, _, _]
+    # ]
+    board.calculate_neighboring_mines()
+
+    # (0, 1), (1, 0) and (1, 1) should each have 1 neighboring mine
+    assert board.board[0][1].neighboring_mines == 1, "Expected 1 neighboring mine"
+    assert board.board[1][0].neighboring_mines == 1, "Expected 1 neighboring mine"
+    assert board.board[1][1].neighboring_mines == 1, "Expected 1 neighboring mine"
+
+    # All other cells should have 0 neighboring mines
+    for i in range(rows):
+        for j in range(columns):
+            if (i, j) not in [(0, 1), (1, 0), (1, 1)]:
+                assert board.board[i][j].neighboring_mines == 0, "Expected 0 neighboring mines"
