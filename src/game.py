1b3fca46a748db20adaf9bf93d9c7a2279c2d20b
VictorApaez <vic.apaez@gmail.com>
2023-08-04 13:26:56 -0400

    created game class to keep track of timer and flag counter

---
+++ src/game.py
@@ -0,0 +1,34 @@
+import time
+from src.board import Board
+class Game:
+    def __init__(self, rows, columns, mines):
+        self.board = Board(rows, columns, mines)
+        self.flags_used = 0
+        self.start_time = None 
+
+    def start_game(self):
+        self.start_time = time.time()
+
+    def reveal_cell(self, row, column):
+        if self.start_time is None:
+            self.start_game()
+        self.board.reveal_cell(row, column)
+
+    def flag_cell(self, row, column):
+        cell = self.board.board[row][column]
+        if cell.is_flagged:  
+            self.flags_used -= 1
+        else:
+            self.flags_used += 1
+        self.board.flag_cell(row, column)
+    
+    def is_game_over(self):
+        return self.board.is_game_over()
+
+    def get_flags_used(self):
+        return self.flags_used
+
+    def get_elapsed_time(self):
+        if self.start_time is None:
+            return 0
+        return time.time() - self.start_time
