6cbf4c91ef4c8639f30da2434dcea7b115fa0c64
VictorApaez <vic.apaez@gmail.com>
2023-08-07 13:39:39 -0400

    updated reveal_cell to check if player has won after revealing a cell

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import tkinter as tk
 from src.game import Game
 from src.cell import MineRevealedError
+from tkinter import messagebox
 
 class MinesweeperGUI:
-    def __init__(self, root, rows=10, columns=10, mines=30):
+    def __init__(self, root, rows=5, columns=5, mines=3):
         self.game = Game(rows, columns, mines)
         self.root = root
         self.buttons = [[None for _ in range(columns)] for _ in range(rows)]
         for i in range(rows):
             for j in range(columns):
-                button = tk.Label(root, text="", bg="white", relief="raised", height=4, width=4)
+                button = tk.Label(root, text="", bg="white", relief="raised", height=2, width=4)
                 button.bind("<Button-1>", lambda event, row=i, col=j: self.reveal_cell(row, col))
                 button.bind("<Button-2>", lambda event, row=i, col=j: self.flag_cell(row, col))
                 button.grid(row=i, column=j)
@@ -21,10 +22,16 @@ class MinesweeperGUI:
         self.update_button_ui(row, col, cell)
 
     def reveal_cell(self, row, col):
-        try:
-            self.game.reveal_cell(row, col, self.update_button_ui)
-        except MineRevealedError:
-            self.end_game()
+      try:
+          self.game.reveal_cell(row, col, self.update_button_ui)
+          if self.game.has_won():
+              self.end_game()
+              self.root.after(100, lambda: messagebox.showinfo("Congratulations", "You Won!"))
+
+      except MineRevealedError:
+          self.end_game()
+          self.root.after(100, lambda: messagebox.showinfo("Game Over", "You Lost!")) 
+
 
     def update_button_ui(self, row, col, cell):
         button = self.buttons[row][col]
@@ -43,6 +50,10 @@ class MinesweeperGUI:
             for j in range(self.game.board.columns):
                 if self.game.board.board[i][j].is_mine:
                     self.buttons[i][j].config(text="M", bg="red")
+
+
+                
+            
 
 root = tk.Tk()
 gui = MinesweeperGUI(root)
